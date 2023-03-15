0e9a93c91394d065c52c68a659d1f1d5c0b57b41
PC13-IFLAB <PC13-IFLAB>
2023-03-15 16:39:19 +0700

    kodebuah

--- MOD4/MOD4/Program.cs
+++ MOD4/MOD4/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("nanananaannaa");
+using System.Net.NetworkInformation;
+
+namespace MOD4
+{
+    internal class program
+    public enum kodeBuah
+    {
+        Apel,
+        Aprikot,
+        Alpukat,
+        Pisang,
+        Paprika,
+        Blackberry,
+        Ceri,
+        Kelapa,
+        Jagung,
+    };
+    public class getkodeBuah
+    {
+        public static string getkode(kodeBuah)
+        {
+            string[] kodeBuah = { "A00", "B00", "C00", "D00", "E00", "F00", "H00", "I00", "J00" };
+            return kodeBuah[(int)kodeBuah];
+        }
+    };
+
+    public static void Main(string[] args)
+    {
+        kodeBuah = kodeBuah.Apel;
+        string kode = getkodeBuah.getkode(kode);
+        Console.WriteLine("kodeBuah : "+ kodeBuah + "\ndengan kodeBuah :"+ kodeBuah);
+        }
+    }
+    
+
